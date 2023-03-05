5f841a35ea82216f4f39469e26e1d2e9126d39cc
adupayra <antoine.dupayrat25@gmail.com>
2023-03-05 03:13:09 +0100

    Added window manager

--- GameOfLife/gui/include/WindowManager.h
+++ GameOfLife/gui/include/WindowManager.h
@@ -1,9 +1,39 @@
 #pragma once
+#include <glad/glad.h>
 #include <glfw3.h>
 
 class WindowManager
 {
 private:
+	GLFWwindow* wind;
+	int width;
+	int height;
+
+	void loadGlad();
+public:
+	WindowManager(int, int, const char*);
+
+	~WindowManager();
+
+	GLFWwindow* getWindow() { return wind; }
+
+	int getWidth() { return width; }
+
+	int getHeight() { return height; }
+
+	static void glfwErrorCallback(int, const char*);
+
+	static void glfwKeyCallback(GLFWwindow*, int, int, int, int);
+
+	static void glfwFrameBuffersizeCallback(GLFWwindow*, int, int);
+
+	void update();
+
+	void processEvents();
+
+	void show();
+
+	bool getCloseState();
 
 };
 
--- GameOfLife/gui/src/WindowManager.cpp
+++ GameOfLife/gui/src/WindowManager.cpp
@@ -1,1 +1,73 @@
 #include "WindowManager.h"
+#include <iostream>
+
+void WindowManager::loadGlad()
+{
+	if (!gladLoadGL()) {
+		std::cerr << "Glad failed to load" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+WindowManager::WindowManager(int width, int height, const char* title)
+{
+	WindowManager::width = width;
+	WindowManager::height = height;
+
+	glfwSetErrorCallback(glfwErrorCallback);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	WindowManager::wind = glfwCreateWindow(width, height, title, NULL, NULL);
+
+	if (!wind)
+		exit(EXIT_FAILURE);
+	glfwSetKeyCallback(wind, glfwKeyCallback);
+	glfwSetFramebufferSizeCallback(wind, glfwFrameBuffersizeCallback);
+
+	glfwMakeContextCurrent(wind);
+
+	loadGlad();
+	
+}
+
+WindowManager::~WindowManager()
+{
+	glfwDestroyWindow(wind);
+	glfwTerminate();
+}
+
+void WindowManager::glfwErrorCallback(int error, const char* desc)
+{
+	std::cerr << "GLFW Error " << desc << " with code " << error << std::endl;
+}
+
+void WindowManager::glfwKeyCallback(GLFWwindow* wind, int key, int scancode, int action, int mods)
+{
+}
+
+void WindowManager::glfwFrameBuffersizeCallback(GLFWwindow* wind, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void WindowManager::update()
+{
+	glfwSwapBuffers(wind);
+}
+
+void WindowManager::processEvents()
+{
+	glfwPollEvents();
+}
+
+void WindowManager::show()
+{
+	glfwShowWindow(wind);
+}
+
+bool WindowManager::getCloseState()
+{
+	return glfwWindowShouldClose(wind);
+}
+
